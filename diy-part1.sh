72b286069a75cbdf8427ebd987540c585155750f
Bruce.carrot93 <carrot.live@hotmail.com>
2023-05-03 20:53:50 +0800

    fix more

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >> feeds.conf.default
