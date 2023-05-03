9532b80dce6ae5c4cd2605c8faf83f5e2a59ae29
Bruce.carrot93 <carrot.live@hotmail.com>
2023-05-03 20:19:25 +0800

    add more

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,10 +15,10 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-# echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >> feeds.conf.default
+echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >> feeds.conf.default
 # echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman.git' >> feeds.conf.default
 # echo 'src-git opentopd https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
