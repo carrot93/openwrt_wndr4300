b2f2e7a8018cbf286a5b96802a914b9ea70c8e81
Bruce.carrot93 <carrot.live@hotmail.com>
2023-05-03 17:14:30 +0800

    add hello

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 # fw876/helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i "/helloworld/d"   feeds.conf.default
-# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
