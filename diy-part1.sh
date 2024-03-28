5db70f6b808916444ad690df56635a4d9ac900f9
Bruce.carrot93 <carrot.live@hotmail.com>
2024-03-28 22:41:32 +0800

    Update c9_3

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
-echo "src-link clash  clash" >> feeds.conf.default
+echo "src-link clash  openwrt-clash" >> feeds.conf.default
 echo "src-link simpleclash luci-app-simple-clash" >> feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
